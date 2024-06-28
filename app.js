c3766ef53e53b9876e697b9aee7ff2c7dd0378be
Roshan pcy <godxrosu@gmail.com>
2024-06-28 19:58:26 +0530

    added

--- app.js
+++ app.js
@@ -19,3 +19,21 @@ var file=require('fs')
 file.readFile('cal.js','utf8',function(error,data){
     console.log(data)
 })
+//creating new file and and added console,log("done") if that cal1.js already exits then  what is there before code inside the cal1.js all code replaced with console.log("done")
+
+// file.writeFile('cal1.js','console.log("done")',function(errror){
+//     console.log('data saved')
+// })
+
+file.writeFile('cal2.js','console.log("done")',function(errror){
+    console.log('data saved')
+})
+
+
+
+//file.appendFile() is used add that code below of line of exiting code 
+ //delete the perticular file
+
+ file.unlink('cal2.js',function(error){
+    console.log('deleted successfully')
+ })
--- cal.js
+++ cal.js
@@ -1,3 +1,4 @@
+console.log("done")
 function add(a,b){
     return a+b;
 }
